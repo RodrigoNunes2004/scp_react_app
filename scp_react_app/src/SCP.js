09417763b47b587612d7205399d872220ee7aa00
Rodrigo De Fraga Nunes <62618446+RodrigoNunes2004@users.noreply.github.com>
2023-09-05 22:56:44 +1200

    Update SCP.js

--- scp_react_app/src/SCP.js
+++ scp_react_app/src/SCP.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function SCP({ scp }) {
-  return ( 
+  return (
     <div>
       <h1>{scp.SCP_subject}</h1>
       <h2>{scp.class}</h2>
-      <img src={scp.image} alt={scp.subject} />
+      <img src={scp.image} alt={scp.subject} className="img-fluid" />
       <p>{scp.paragraph_1}</p>
       <p>{scp.description}</p>
       <p>{scp.paragraph_2}</p>
